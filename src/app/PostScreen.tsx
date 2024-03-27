3cec777fe7457330e9620533b2a25375b31547d4
Emeka Okezie <eokezie@nasdng.com>
2024-03-27 19:04:21 +0100

    feat: worked on useSWRMutation for sending data

--- src/app/PostScreen.tsx
+++ src/app/PostScreen.tsx
@@ -1,10 +1,39 @@
-import { ActivityIndicator, Text, FlatList } from "react-native";
+import { ActivityIndicator, Text, FlatList, Button, Alert } from "react-native";
+// import { useSWRConfig } from "swr";
 
-import { usePosts } from "../hooks/usePost";
+import { usePosts, useCreatePost } from "../hooks/usePost";
+import { PostDataType } from "../hooks/usePost";
 import PostListitems from "../components/PostListitems";
 
 export default function App() {
-  const { error, isLoading, post } = usePosts();
+  const { error, isLoading, post, mutate } = usePosts();
+
+  const { trigger, newPost } = useCreatePost();
+
+  const runMutate = () => {
+    mutate();
+  };
+
+  const onCreatePost = async () => {
+    const newData: PostDataType | any = {
+      title: "Title",
+      body: "Post body content",
+      userid: 2,
+    };
+
+    try {
+      await trigger(newData, {
+        optimisticData: (current) => {
+          return [newData, ...current];
+        },
+        revalidate: false,
+      });
+    } catch (e) {
+      Alert.alert("Failed to create post");
+    }
+  };
+
+  console.log(newPost);
 
   if (isLoading) {
     return <ActivityIndicator />;
@@ -15,16 +44,20 @@ export default function App() {
   }
 
   return (
-    <FlatList
-      data={post}
-      contentContainerStyle={{ gap: 10, padding: 10 }}
-      renderItem={({ item }: any) => (
-        <PostListitems
-          title={item.title}
-          body={item.body}
-          userId={item.userId}
-        />
-      )}
-    />
+    <>
+      <Button title="Refresh" onPress={runMutate} />
+      <Button title="Create Post" onPress={onCreatePost} />
+      <FlatList
+        data={post}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        renderItem={({ item }: any) => (
+          <PostListitems
+            title={item.title}
+            body={item.body}
+            userId={item.userId}
+          />
+        )}
+      />
+    </>
   );
 }
--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -1,13 +1,48 @@
 import useSWR from "swr";
+import useSWRMutation from "swr/mutation";
+
+export type PostDataType = {
+  title: string;
+  body: string;
+  userid: number;
+};
 
 export function usePosts() {
-  const { data, isLoading, error } = useSWR(
-    "https://jsonplaceholder.typicode.com/posts "
+  const { data, isLoading, error, mutate } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts"
   );
 
   return {
     post: data,
     isLoading,
+    error,
+    mutate,
+  };
+}
+
+// Creating a new post
+const createPost = async (url: string, postData: PostDataType) => {
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  });
+
+  return response.json();
+};
+
+export function useCreatePost() {
+  const { trigger, data, isMutating, error } = useSWRMutation(
+    "https://jsonplaceholder.typicode.com/posts",
+    createPost
+  );
+
+  return {
+    trigger,
+    newPost: data,
+    isMutating,
     error,
   };
 }
